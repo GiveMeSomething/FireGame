9622a63b59caf7e48c29f996b97e32716e6b0994
Hoang Minh <minhbeo135@gmail.com>
2018-07-31 11:52:44 +0700

    Add Sound To Alarm page

--- Project101/Assets/MainProject/Scripts/SceneController/BookManager.cs
+++ Project101/Assets/MainProject/Scripts/SceneController/BookManager.cs
@@ -11,6 +11,8 @@ public class BookManager : MonoBehaviour {
     private Image leftNext;
     private Image rightNext;
     public Text guideText;
+    public Button playSound;
+    public AudioSource audio;
 
     private void Start()
     {
@@ -18,6 +20,9 @@ public class BookManager : MonoBehaviour {
         rightNext = book.RightNext;
 
         backButton.SetActive(false);
+        playSound.gameObject.SetActive(false);
+        audio = GetComponent<AudioSource>();
+        audio.Stop();
     }
     // Update is called once per frame
     void Update () {
@@ -33,7 +38,7 @@ public class BookManager : MonoBehaviour {
             guideText.gameObject.SetActive(true);
         }
 
-        if (rightNext.sprite.name == "BlankPage")
+        if (rightNext.sprite.name.Equals("BlankPage"))
         {
             backButton.SetActive(true);
         }
@@ -41,11 +46,22 @@ public class BookManager : MonoBehaviour {
         {
             backButton.SetActive(false);
         }
+
+        if (rightNext.sprite.name.Equals("FireBook-18"))
+        {
+            playSound.gameObject.SetActive(true);
+        }
     }
 
     public void BackToMenu()
     {
         SceneManager.LoadScene("MainMenu");
     }
+
+    public void PlaySound()
+    {
+        audio.Play();
+    }
+
     
 }
