bbdbf902726e4c20ac319aa166fb98f0ca585fff
Hoang Minh <minhbeo135@gmail.com>
2018-07-31 11:21:41 +0700

    Display out questions

--- Project101/Assets/MainProject/Scripts/DialougueManager/DialogueManagerRoom.cs
+++ Project101/Assets/MainProject/Scripts/DialougueManager/DialogueManagerRoom.cs
@@ -18,18 +18,49 @@ public class DialogueManagerRoom : MonoBehaviour
 
     public Animator animator;
     public Animator hintBox;
+    public GameObject controller;
+
+    private bool clickable = false;
 
     // Use this for initialization
     void Start()
     {
-
+        choiceOne.gameObject.SetActive(false);
+        choiceTwo.gameObject.SetActive(false);
     }
 
     public void StartDialogueRoom(Dialogue dialogue)
     {
+        choiceOne.gameObject.SetActive(false);
+        choiceTwo.gameObject.SetActive(false);
+
         animator.SetBool("isOpen", true);
 
         nameText.text = dialogue.name;
+
+        questions.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            questions.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void StartDialogueRoom(Dialogue dialogue, string rightAnswer, string wrongAnswer, string hint)
+    {
+        choiceOne.gameObject.SetActive(true);
+        choiceTwo.gameObject.SetActive(true);
+
+        counter++;
+
+        animator.SetBool("isOpen", true);
+
+        nameText.text = dialogue.name;
+
+        choiceOne.GetComponentInChildren<Text>().text = rightAnswer;
+        choiceTwo.GetComponentInChildren<Text>().text = wrongAnswer;
 
         questions.Clear();
 
@@ -45,7 +76,8 @@ public class DialogueManagerRoom : MonoBehaviour
     {
         if (questions.Count == 0)
         {
-            EndDialogue();
+            PlayerStartDialogue dialogue = controller.GetComponent<PlayerStartDialogue>();
+            dialogue.NextQuestion(counter);
             return;
         }
         string sentence = questions.Dequeue();
--- Project101/Assets/MainProject/Scripts/Player/PlayerStartDialogue.cs
+++ Project101/Assets/MainProject/Scripts/Player/PlayerStartDialogue.cs
@@ -9,9 +9,17 @@ public class PlayerStartDialogue : MonoBehaviour {
     public Button startButton;
     
     private string[] startDialogue = { "Về đến nhà rồi. Mình cùng ôn lại một bài học nhé." };
-    
-	void Start () {
-		
+    ArrayList questionlist = new ArrayList();
+    private string[] question1 = { "Q1" };
+    private string[] question2 = { "Q2" };
+    private string[] rightAnswer = { "rA1", "rA2", "rA3" };
+    private string[] wrongAnswer = { "wA1", "wA2", "wA3" };
+    private string[] hint = { "h1", "h2", "h3" };
+    private int numberQuestions = 0;
+
+    void Start () {
+        questionlist.Add(question1);
+        questionlist.Add(question2);
 	}
 	
 	// Update is called once per frame
@@ -25,4 +33,20 @@ public class PlayerStartDialogue : MonoBehaviour {
         started = true;
         FindObjectOfType<DialogueManagerRoom>().StartDialogueRoom(dialogue);
 	}
+
+    public void NextQuestion(int number)
+    {
+        Dialogue dialogue = new Dialogue();
+
+        dialogue.name = "Ken";
+        dialogue.sentences = (string[]) questionlist[number];
+        string right = rightAnswer[number];
+        string wrong = wrongAnswer[number];
+        string hintBox = hint[number];
+
+        started = true;
+        FindObjectOfType<DialogueManagerRoom>().StartDialogueRoom(dialogue, right, wrong, hintBox);
+    }
+
+
 }
